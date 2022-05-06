85e7db7d3534bbc50c3b7f6c20b0648b40c9fd8d
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-05-06 16:17:41 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, {useState} from "react";
+import { Switch, Route, Redirect, NavLink } from "react-router-dom";
+
+
+import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
+import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+
+function App() {
+  /*
+  Define state variables for 
+  contacts and appointments 
+  */
+ const [contacts, setContacts] = useState([]);
+ const [appointments, setAppointments] = useState([]);
+
+  const ROUTES = {
+    CONTACTS: "/contacts",
+    APPOINTMENTS: "/appointments",
+  };
+
+  /*
+  Implement functions to add data to
+  contacts and appointments
+  */
+//  add contact
+ const addContact = (newContact) => {
+   const contact = {
+     name: newContact.name,
+     phone: newContact.phone,
+     email: newContact.email
+   }
+
+   setContacts((prev)=>{
+     const newContactsArray = [...prev, contact];
+     return newContactsArray;
+   })
+ }
+
+// add appointment
+const addAppointment = (newAppointment) => {
+  const appointment = {
+    title: newAppointment.title,
+    contact: newAppointment.contact,
+    date: newAppointment.date,
+    time: newAppointment.time
+  }
+
+  setAppointments((prev)=>{
+    const newAppointmentsArray = [...prev, appointment];
+    return newAppointmentsArray;
+  })
+}
+
+  return (
+    <>
+      <nav>
+        <NavLink to={ROUTES.CONTACTS} activeClassName="active">
+          Contacts
+        </NavLink>
+        <NavLink to={ROUTES.APPOINTMENTS} activeClassName="active">
+          Appointments
+        </NavLink>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to={ROUTES.CONTACTS} />
+          </Route>
+          <Route path={ROUTES.CONTACTS}>
+            <ContactsPage 
+              contacts={contacts} 
+              addContact={addContact}/>
+          </Route>
+          <Route path={ROUTES.APPOINTMENTS}>
+            <AppointmentsPage 
+              appointments={appointments} 
+              addAppointment={addAppointment} />
+          </Route>
+        </Switch>
+      </main>
+    </>
+  );
+}
+
+export default App;
